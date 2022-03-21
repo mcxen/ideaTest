c94d03a3aaa75ac40784e60ae1f93ddb50ca4db5
mcxen <shewhen@qq.com>
2022-03-21 20:59:26 +0800

    2022年3月21日

---
+++ Test/src/test.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+/**
+ * 2022/3/21
+ * mcxen
+ * ideaTest
+ *
+ * @author McXen
+ **/
+public class test {
+    public static void main(String[] args) {
+        HashMap<String , String > map = new HashMap<>();
+        map.put("chenxianwen","Tom");
+        map.put("age","12");
+        map.put("eamil","c@2.com");
+        System.out.println(map);
+
+    }
+}
